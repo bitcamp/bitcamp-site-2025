7bcec2f29d5bd0fd069181580d984854f5371a3a
Arya010905 <aryagijare@gmail.com>
2025-01-17 10:25:26 -0600

    test3

--- components/CampGamesPage.vue
+++ components/CampGamesPage.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="gradient">Campfire Games
+    <div class="gradient">
+        Campfire Games Page
+        <div class="image-container">
+            <img src="../assets/img/images/marshies.svg" class="dinos">
+        </div>
     </div>
 </template>
 
@@ -12,7 +16,78 @@ export default {
 <style scoped>
 .gradient {
     background-color: #ff5b01;
+    background-image: url('../assets/img/images/Brownandpurplecaves.svg');  
+    background-size: cover;  
+    background-position: center -50px;
     display: flex;
-    height: 40rem;
+    height: 170vh; 
+    flex-direction: column;
+    position: relative;
 }
+
+.image-container {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.dinos {
+    width: 110rem;
+    /* transform: translate(-50%, -50%); */
+}
+
+/* For mobile screens */
+@media screen and (max-width: 768px) {
+    .gradient {
+        background-image: url('../assets/img/images/Browncavestretched.svg');
+        background-size: cover;
+        background-position: center center;
+        height: 380rem
+    }
+    
+    /* Adjust dino positions for smaller screens */
+    .overlay-blue-dino {
+        top: 55%;
+        left: 50%;            
+        transform: translate(-50%, -50%);
+    }
+
+    .overlay-red-dino {
+        top: 30%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    .overlay-green-dino {
+        top: 80%;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+
+    .overlay-blue-blurb {
+        top: 48%;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+
+    .overlay-green-blurb {
+        top: 74%;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+
+    .overlay-red-blurb {
+        visibility: hidden;
+    }
+
+    .overlay-red-blurb-mv {
+        position: absolute;           
+        top: 23%;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+}
+
 </style>
