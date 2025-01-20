79b64d1a52aded953b5202a139cf0067723815fb
Swathi Pulipati <79487741+SwathiPulipati@users.noreply.github.com>
2025-01-19 22:34:42 -0500

    added faq title, faq questions, and sponsor title

--- components/FAQSponsorPage.vue
+++ components/FAQSponsorPage.vue
@@ -1,18 +1,473 @@
 <template>
-    <div class="gradient">FAQ and Sponsor Page
+    <div id="faq" class="section">
+        <div class="top-cliffs-div">
+            <img src="../assets/img/faq/faq_sponsor_background_top.png" class="top-cliffs"/>
+        </div>
+        <div class="content-wrapper">
+            <div class="faq-header">
+                <div class="faq-rock-div">
+                    <img src="~/assets/img/faq/faq_rock.svg" class="faq-rock-main"/>
+                    <div class="faq-rock-shadow-div">
+                        <img src="~/assets/img/faq/faq_rock_shadow.svg" class="faq-rock-shadow"/>
+                    </div>
+                    <div class="faq-text-div">
+                        <img src="~/assets/img/faq/faq_text.svg" class="faq-text"/>
+                    </div>
+                </div>
+            </div>
+            <div class="faq-contents">
+                <div class="Question_Wrapper">
+                    <div
+                    v-for="(questions, i) in [questions_left, questions_right]"
+                    :key="i"
+                    class="Question_Column"
+                    >
+                        <div
+                            v-for="(faq, i) in questions"
+                            :key="i"
+                            class="Question"
+                            :faq="faq"
+                        >
+                            <button
+                            class="Question_Button"
+                            :class="{ opened: faq.question === currentOpenedQuestion }"
+                            @click="toggleButton(faq.question)"
+                            >
+                            {{ faq.question }}
+                            </button>
+                            <div
+                            :class="
+                                faq.question === currentOpenedQuestion
+                                ? 'Answer_Opened'
+                                : 'Answer'
+                            "
+                            v-html="faq.answer"
+                            ></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="sponsor-header">
+                <div class="sponsor-rock-div">
+                    <img src="~/assets/img/faq/sponsor_rock.svg" class="sponsor-rock-main"/>
+                    <div class="sponsor-rock-shadow-div">
+                        <img src="~/assets/img/faq/sponsor_rock_shadow.svg" class="sponsor-rock-shadow"/>
+                    </div>
+                    <div class="sponsor-text-div">
+                        <img src="~/assets/img/faq/sponsor_text.svg" class="sponsor-text"/>
+                    </div>
+                </div>
+            </div>
+            <div class="sponsor-contents"></div>
+        </div> 
+        <div class="bottom-cliffs-div">
+            <img src="../assets/img/faq/faq_sponsor_background_bottom.png" class="bottom-cliffs"/>
+        </div>  
     </div>
 </template>
-
-<script lang="ts">
-export default {
-    name: 'PastEventsGrid',
-};
+  
+<script setup lang="ts">
+  import { ref } from 'vue';
+  interface FAQ {
+    question: string;
+    answer: string;
+  }
+  const questions = ref<FAQ[]>([
+    {
+      question: 'What is Bitcamp?',
+      answer:
+        "Bitcamp is a hackathon that values participant experience and mentorship over competitiveness and points. Come to have fun with your friends, learn something new, eat s'mores, and have a generally awesome time. We have all sorts of crazy activities planned for you...come find out the rest!",
+    },
+    {
+      question: "What's a hackathon?",
+      answer:
+        "A hackathon is a creative marathon all about building something cool. Students are encouraged to come up with an idea, form teams, and then build out that idea (typically through programming!) into a product in 36 hours. We want you to take something you love (sports, art, camping, anything!) and combine it with technology to make something awesome. It's a great time to push the envelope and learn some new skills.",
+    },
+    {
+      question: 'Do I have to be experienced to attend?',
+      answer:
+        'No prior experience is required to attend Bitcamp. Exciting workshops and helpful mentors will give you the resources to help you build your dream project. Just come with your head and a willingness to learn!',
+    },
+    {
+      question: "Is it okay if I don't have an idea or team?",
+      answer:
+        'No idea? No team? No problem! There will be dedicated events during Bitcamp for idea creation and team formations.',
+    },
+    {
+      question: "Can I attend if I'm a minor (under 18)?",
+      answer:
+        'Yes! Minors can attend with a chaperone, and can apply <a class="link" href="https://register.bit.camp" target="_blank">here</a>!',
+    },
+    {
+      question: "Can I attend if I don't want to participate in hacking?",
+      answer:
+        "Although Bitcamp is a hackathon, there is no requirement to hack if you don't want to. If hacking isn't your thing, you can still participate in our exciting workshops and fun mini-events.",
+    },
+    {
+      question: 'How else can I get involved?',
+      answer:
+        "We'd love to get you on our volunteering or mentoring teams! If you'd like to help, please register as a <a class='link' href='https://register.bit.camp/mentor' target='_blank'>mentor</a> or <a class='link' href='https://register.bit.camp/volunteer' target='_blank'>volunteer</a>. Also, be sure to follow us on Facebook and Twitter for updates!",
+    },
+    {
+      question: 'How do I join a team?',
+      answer:
+        "Projects are submitted by teams to DevPost. You don't need to finalize your team until project submissions are due during the event. You may work individually or in a team of up to four campers. Don't have a team in mind? No problem! Bitcamp will kick off with an optional team formation event.",
+    },
+    {
+      question: 'How competitive is the application process?',
+      answer:
+        'It’s not competitive at all! The application process primarily serves as a way for us to verify application details as they come in. Unless you receive any contact from us (other than the confirmation email), we’ll see you by the campfire!',
+    },
+    {
+      question: 'Who can apply to Bitcamp?',
+      answer:
+        'Any college student, high school student, or middle school student is more than welcome to apply to Bitcamp!',
+    },
+    {
+      question: 'Is Bitcamp free to attend?',
+      answer: 'Yes! There is no cost to attend Bitcamp.',
+    },
+    {
+      question: 'Can people not registered for Bitcamp attend?',
+      answer:
+        'People not registered for Bitcamp will not be allowed entrance to the hackathon.',
+    },
+    {
+      question: 'What have people made in the past at Bitcamp?',
+      answer:
+        'From Augmented Reality Human-Scale Pong to a research paper detailing vulnerabilities in Google\'s reCaptcha system, the projects at Bitcamp span across all categories and interests. You can check out all of the amazing submissions at the <a class="link" href="https://bitcamp2023.devpost.com/project-gallery"> Bitcamp 2023 Devpost!</a>', //ADD LINK
+    },
+    {
+      question: 'What hardware is provided at Bitcamp?',
+      answer:
+        'Arduinos, sensors (ultrasonic, photoresistors, thermistors), inputs (buttons, switches), outputs (LEDs, piezo speakers, 7-segment displays, micro servo motors), passive components (resistors, capacitors, diodes), and wiring.',
+    },
+    {
+      question: "Does Bitcamp provide travel accomodations or reimbursement?",
+      answer: 'We are currently gauging interest for a new travel reimbursement application! If you’re interested, please fill out <a class="link" href="https://forms.gle/nX2NuooijXav2qgr6">this form</a>. In addition, feel free to contact <a class="link" href="mailto:travel@bit.camp">travel@bit.camp</a> with any additional questions!',
+    },
+    {
+      question: 'Do I need to wear a mask?',
+      answer:
+        'No, attendees are not required to wear a mask. However, feel free to wear a mask if you would like!',
+    },
+  ]);
+  const questions_left = questions.value.slice(0, questions.value.length / 2);
+  const questions_right = questions.value.slice(
+    questions.value.length / 2,
+    questions.value.length
+  );
+  const currentOpenedQuestion = ref<string | null>(null);
+  function toggleButton(question: string) {
+    if (currentOpenedQuestion.value === question) {
+      currentOpenedQuestion.value = null;
+    } else {
+      currentOpenedQuestion.value = question;
+    }
+  }
 </script>
+  
+<style scoped lang="scss">
+  .section {
+    // background-image: url("~/assets/img/faq/faq_sponsor_background_middle.png");
+    // padding-top: 157.75623%;
+    // background-size: cover !important;
+    // background-attachment: local;
+    // width: 100vw;
+    // height: ;
+    // width: 100vw;
+    // height: 100vw;
+    flex-shrink: 0;
+    position: relative;
+  }
 
-<style scoped>
-.gradient {
-    background-color: #ff5b01;
+  .top-cliffs {
+    width: 100%;
+    margin-bottom: -10px;;
+  }
+
+  .bottom-cliffs {
+    width: 100%;
+    margin-bottom: -10px;;
+  }
+
+  .content-wrapper {
+    background-image: url("~/assets/img/faq/faq_sponsor_background_middle.png");
+    background-size: 100% 100%;
+    border-top: 2px solid #000;    
+    // idk why corder-top fixes the gradient continuity but it does
+    // width: 100vw;
+    // height: 100%;
+    // margin-top: -142.75623%;
+  }
+
+  .faq-rock-div {
+    position: relative;
+    // width: 528px;
+    // height: 176px;
+    width: 36.5%;
+    flex-shrink: 0;
+    margin-top: -30%;
+    margin-left: 28.53185%;
+    
+  }
+
+  .faq-rock-main {
+    fill: #560001;
+  }
+
+  .faq-rock-shadow-div {
+    position: absolute;
+    // width: 399px;
+    width: 75.56818%;
+    // height: 60.5px;
+    height: 34.375%;
+    flex-shrink: 0;
+    margin-top: -18%;
+    margin-left: 13.2574%;
+  }
+
+  .faq-rock-shadow {
+    fill: #8B2000;
+  }
+
+  .faq-text-div {
+    width: 42.8030303%;
+    margin-top: -28%;
+    margin-left: 40.90909%;
+    z-index: 2;
+    position: relative;
+  }
+
+  .sponsor-rock-div {
+    position: relative;
+    // width: 528px;
+    // height: 176px;
+    width: 57.409972299%;
+    flex-shrink: 0;
+    margin-top: -1%;
+    margin-left: 20.637119113%;
+    
+  }
+
+  .sponsor-rock-main {
+    // opacity: .5;
+    fill: #8B2000;
+  }
+
+  .sponsor-rock-shadow-div {
+    position: absolute;
+    // width: 399px;
+    width: 99.5%;
+    // height: 60.5px;
+    height: 73.076923%;
+    flex-shrink: 0;
+
+
+    @media (max-width: 767.8px) {
+        margin-top: -29.5%;
+        margin-left: 0.2309638%;
+    }
+
+    @media (min-width: 768px) and (max-width: 959.8px) {
+        margin-top: -29%;
+        margin-left: 0.2309638%;
+    }
+
+    @media (min-width: 960px) and (max-width: 1199.98px) {
+        margin-top: -28.5%;
+        margin-left: 0.2309638%;
+    }
+
+    @media (min-width: 1200px) {
+        margin-top: -28%;
+        margin-left: 0.2309638%;
+    }
+  }
+
+  .sponsor-rock-shadow {
+    // opacity: .5;
+    fill: #560001;  
+  }
+
+  .sponsor-text-div {
+    position: absolute;
+    width: 67.395657418%;
+    margin-top: -21%;
+    // margin-left: 21.95416164%;
+    margin-left: 21.95416164%;
+    z-index: 2;
+  }
+
+  .sponsor-header img {
+    width: 100%;
+    height: auto;
+  }
+  
+  .faq-contents {
+    position: relative;
+    font-family: 'Avenir';
+    font-style: normal;
+    font-weight: 500;
+    border-radius: 15px;
+    padding: 4vw;
+    overflow-y: visible;
+    aspect-ratio: 10;
+    background-size: cover !important;
+    // margin: 20px;
+    box-shadow: 0, 0, 20px, 0 rgba(24, 22, 81, 0.2);
+    z-index: 10;
+    text-align: center;
+    // background-color: #5eb8ba;
+    // background-color: #ABD2D3;
+    // background-color: #354C4D;
+    // background-color: #3D6982;
+    // background-color: #256871;
+    background-color: #E23E00;
+    backdrop-filter: blur(35px);
+    // margin: 8vw;
+    margin-left: 11vw;
+    margin-right: 11vw;
+    margin-bottom: 0;
+  }
+  
+  .faq-header img {
+    width: 100%;
+    height: auto;
+  }
+  
+  .Question_Wrapper {
     display: flex;
-    height: 40rem;
-}
+    flex-wrap: wrap;
+  }
+  
+  .Question_Column {
+    flex: 1;
+  }
+  
+  .Question {
+    display: block;
+    padding-bottom: 2px;
+    padding-left: 10px;
+    padding-right: 10px;
+    border-radius: 12px;
+    font-family: 'Avenir';
+    font-style: normal;
+    font-weight: 500;
+    color: white;
+    width: 100%;
+    background-color: transparent;
+    border-bottom-style: solid rgba(151, 29, 29, 0.2);
+  
+    overflow: hidden;
+  }
+  
+  .Question_Button {
+    &::after {
+      content: url(../assets/img/icons/plus.svg);
+      min-width: 4rem;
+      min-height: 4rem;
+      max-width: 10rem;
+      max-height: 10rem;
+    //   width: 4rem;
+    //   height: 4rem;
+      margin-top: 1rem;
+      margin-right: 1rem;
+      align-items: end;
+      transform-origin: center;
+      transform: rotateZ(0deg);
+      transition: all 0.4s ease-out;
+    }
+  
+    &.opened {
+      &::after {
+        transform-origin: center;
+        transform: rotateZ(90deg);
+      }
+    }
+  
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1rem 1rem;
+    border-left: transparent;
+    border-right: transparent;
+    border-top: transparent;
+    border-bottom: 2px solid white;
+    background: transparent;
+    padding: 8px 12px;
+    border-radius: 0px;
+    text-emphasis: bold;
+    text-align: left;
+    font-family: 'Avenir';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 1.7vw;
+    line-height: 1.3;
+    font-weight: bold;
+    color: white;
+    overflow: hidden;
+    height: 6vw;
+    width: 100%;
+    padding: 3rem 12px 2.7rem;
+    cursor: pointer;
+  }
+  
+  .Answer {
+    padding: 0px 12px;
+    text-align: left;
+    font-size: 1.4vw;
+    justify-content: top;
+    max-height: 0px;
+    overflow: hidden;
+    transition: max-height 0.6s ease-in-out;
+  }
+  
+  .Answer_Opened {
+    padding: 0px 12px;
+    padding-top: 1rem;
+    text-align: left;
+    font-size: 1.4vw;
+    justify-content: top;
+    overflow-y: auto;
+    transition: max-height 0.6s ease-in-out;
+    margin-bottom: 2rem;
+  }
+  
+  @media (max-width: 768px) {
+    .Question_Column {
+      min-width: 100%;
+    }
+  
+    .Question,
+    .Question_Button {
+      font-size: 3vw;
+    }
+  
+    .Question_Button {
+      height: 10vw;
+      padding: 2rem 12px 1.75rem;
+    }
+  
+    .Answer,
+    .Answer_Opened {
+      height: 100%;
+      font-size: 2.5vw;
+    }
+  }
+  
 </style>
+  
+<style lang="scss">
+  a.link {
+    // color: var(--color-bitcamp);
+    color: #8B2000;;
+  }
+  
+  ul {
+    margin-block-start: 1rem;
+    margin-block-end: 1rem;
+    padding-inline-start: 1rem;
+  }
+</style>
+  
