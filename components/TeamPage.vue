5329ca5850ddf9e1384b21256b4b9ca0141b2982
Sai Pranav Theerthala <sptheerthala@gmail.com>
2025-02-04 20:30:30 -0500

    completed our team page

--- components/TeamPage.vue
+++ components/TeamPage.vue
@@ -1,5 +1,33 @@
 <template>
-    <div class="gradient">Our Team Page
+    <div class="gradient">
+        <div class="page_container">
+            <div class="page_header">
+                <img class="header_image" src="../assets/img/images/our_team_img.svg" alt="lebron" />
+            </div>
+            <div class="mid_content_container">
+                <div class="team_image">
+                    <img class="team_photo" src="../assets/img/images/team_photo_background.svg" alt="lebron" />
+                </div>
+                <div class="message">
+                    <div class="message_text">
+                        We're the co-executive directors of Bitcamp 2025, and we're so excited to be working with this
+                        amazing team to lead Bitcamp into a new decade of hackathon culture. Each and every single one
+                        of
+                        our 82 organizers plays an integral part in making Bitcamp happen, and we can't wait to see
+                        these
+                        brilliant minds bring Bitcamp 2025 to life and make it possible for hackers like you to Unearth
+                        Your
+                        Potential!
+                        <br><br> - Anjali Samavedam and Sharvari Tirodkar
+                    </div>
+                </div>
+
+            </div>
+            <div class="bottom_button_container">
+                <img class="meet_team_image" src="../assets/img/images/meet_team_button.svg" alt="lebron" />
+            </div>
+
+        </div>
     </div>
 </template>
 
@@ -11,8 +39,49 @@ export default {
 
 <style scoped>
 .gradient {
-    background-color: #ff5b01;
+    background-image: url(assets/img/images/purplecaves.svg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    /* height: 100vh; */
+}
+
+.page_container {
     display: flex;
-    height: 40rem;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 4rem;
+}
+
+.mid_content_container {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 100%;
+}
+
+.team_image {
+    margin-inline: 7.5%;
+}
+
+.team_photo {
+    width: 90%;
+}
+
+.message {
+    background-image: url(../assets/img/images/message_background.svg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    width: 55rem;
+    display: flex;
+    flex-wrap: wrap;
+    align-content: center;
+}
+
+.message_text {
+    margin-left: 12%;
+    margin-right: 2%;
+    text-align: center;
 }
 </style>
