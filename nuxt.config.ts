2db1307d8986ba943a936858a3c5ed8f64ef77d4
Sai Pranav Theerthala <sptheerthala@gmail.com>
2025-02-06 12:35:43 -0500

    updated nuxt file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,19 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
+
+  nitro: {
+    preset: 'netlify'
+  },
 
   typescript: {
     strict: true,
   },
 
-  css: ['~/assets/css/global.css', '~/public/bitcamp-brand/bitcamp.css'],
+  css: [
+    '~/assets/css/global.css',
+    '~/public/bitcamp-brand/bitcamp.css'
+  ],
   compatibilityDate: '2024-09-13',
 });
