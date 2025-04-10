4e2f363a88645c551eee24e882cff80cceeaa95e
Eric Chen <ericchen314@gmail.com>
2025-04-10 10:52:14 -0400

    Fix favicon

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,4 @@
 <template>
-  <link rel="icon" href="bitcamp-site-2025/public/bitcamp-brand/favicons/favicon-16x16.png">
   <div class="wrapper" ref="el">
     <div class="app-container">
       <Navbar />
@@ -16,6 +15,96 @@
   </div>
 </template>
 
+
+<script lang="ts" setup>
+useHead({
+  title: 'Bitcamp',
+  meta: [
+    {
+      name: 'description',
+      content:
+        "Bitcamp is a place for exploration. You will have 36 hours to delve into your curiosities, learn something new, and make something awesome. With world-class mentors and hundreds of fellow campers, you're in for an amazing time. If you're ready for an adventure, see you by the fire!",
+    },
+    {
+      property: 'og:title',
+      content: 'Bitcamp 2025',
+    },
+    {
+      property: 'og:site_name',
+      content: 'Bitcamp 2025',
+    },
+    {
+      property: 'og:url',
+      content: 'https://bit.camp/',
+    },
+    {
+      property: 'og:description',
+      content:
+        "Bitcamp is a place for exploration. You will have 36 hours to delve into your curiosities, learn something new, and make something awesome. With world-class mentors and hundreds of fellow campers, you're in for an amazing time. If you're ready for an adventure, see you by the fire!",
+    },
+    {
+      property: 'og:type',
+      content: 'website',
+    },
+    {
+      property: 'twitter:card',
+      content: 'summary',
+    },
+    {
+      property: 'twitter:url',
+      content: 'https://bit.camp/',
+    },
+    {
+      property: 'twitter:title',
+      content: 'Bitcamp 2025',
+    },
+    {
+      property: 'twitter:description',
+      content:
+        "Bitcamp is a place for exploration. You will have 36 hours to delve into your curiosities, learn something new, and make something awesome. With world-class mentors and hundreds of fellow campers, you're in for an amazing time. If you're ready for an adventure, see you by the fire!",
+    },
+    {
+      name: 'msapplication-TileColor',
+      content: '#ff6f3f',
+    },
+    {
+      name: 'msapplication-config',
+      content: '/bitcamp-brand/favicons/browserconfig.xml',
+    },
+    {
+      name: 'theme-color',
+      content: '#ffffff',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/bitcamp-brand/favicons/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/bitcamp-brand/favicons/favicon-16x16.png',
+    },
+    {
+      rel: 'manifest',
+      href: '/bitcamp-brand/favicons/site.webmanifest',
+    },
+    {
+      rel: 'mask-icon',
+      href: '/bitcamp-brand/favicons/safari-pinned-tab.svg',
+      color: '#ff6f3f',
+    },
+    {
+      rel: 'shortcut icon',
+      href: '/bitcamp-brand/favicons/favicon.ico',
+    },
+  ],
+});
+</script>
 <script lang="ts">
 // import { Break } from '#build/components';
 import Navbar from '~/components/Navbar.vue';
@@ -32,96 +121,6 @@ export default {
   name: 'HomePage',
   // components: { Navbar, FooterContent, LandingPage, Break, TracksPage, CampGamesPage, TeamPage, FAQSponsorPage },
   components: { Navbar, Schedule },
-
-  head() {
-    return {
-      title: 'Bitcamp',
-      meta: [
-        {
-          name: 'description',
-          content:
-            "Bitcamp is a place for exploration. You will have 36 hours to delve into your curiosities, learn something new, and make something awesome. With world-class mentors and hundreds of fellow campers, you're in for an amazing time. If you're ready for an adventure, see you by the fire!",
-        },
-        {
-          property: 'og:title',
-          content: 'Bitcamp 2025',
-        },
-        {
-          property: 'og:site_name',
-          content: 'Bitcamp 2025',
-        },
-        {
-          property: 'og:url',
-          content: 'https://bit.camp/',
-        },
-        {
-          property: 'og:description',
-          content:
-            "Bitcamp is a place for exploration. You will have 36 hours to delve into your curiosities, learn something new, and make something awesome. With world-class mentors and hundreds of fellow campers, you're in for an amazing time. If you're ready for an adventure, see you by the fire!",
-        },
-        {
-          property: 'og:type',
-          content: 'website',
-        },
-        {
-          property: 'twitter:card',
-          content: 'summary',
-        },
-        {
-          property: 'twitter:url',
-          content: 'https://bit.camp/',
-        },
-        {
-          property: 'twitter:title',
-          content: 'Bitcamp 2025',
-        },
-        {
-          property: 'twitter:description',
-          content:
-            "Bitcamp is a place for exploration. You will have 36 hours to delve into your curiosities, learn something new, and make something awesome. With world-class mentors and hundreds of fellow campers, you're in for an amazing time. If you're ready for an adventure, see you by the fire!",
-        },
-        {
-          name: 'msapplication-TileColor',
-          content: '#ff6f3f',
-        },
-        {
-          name: 'msapplication-config',
-          content: '/bitcamp-brand/favicons/browserconfig.xml',
-        },
-        {
-          name: 'theme-color',
-          content: '#ffffff',
-        },
-      ],
-      link: [
-        {
-          rel: 'icon',
-          type: 'image/png',
-          sizes: '32x32',
-          href: '/bitcamp-brand/favicons/favicon-32x32.png', 
-        },
-        {
-          rel: 'icon',
-          type: 'image/png',
-          sizes: '16x16',
-          href: '/bitcamp-brand/favicons/favicon-16x16.png', 
-        },
-        {
-          rel: 'manifest',
-          href: '/bitcamp-brand/favicons/site.webmanifest', 
-        },
-        {
-          rel: 'mask-icon',
-          href: '/bitcamp-brand/favicons/safari-pinned-tab.svg', 
-          color: '#ff6f3f',
-        },
-        {
-          rel: 'shortcut icon',
-          href: '/bitcamp-brand/favicons/favicon.ico',
-        },
-      ],
-    };
-  },
 };
 </script>
 
